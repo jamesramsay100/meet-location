50371605010e13ff347a213eecf32d816aa874dd
James Ramsay <james.ramsay@lytt.com>
2022-01-17 23:05:20 +0000

    refactor address form

---
+++ src/components/AddressForm.js
@@ -0,0 +1,77 @@
+import * as React from 'react';
+import { useState } from 'react';
+import Container from '@mui/material/Container';
+import Typography from '@mui/material/Typography';
+import Box from '@mui/material/Box';
+import Link from '@mui/material/Link';
+import Grid from '@mui/material/Grid';
+import Input from '@mui/material/Input';
+import { TextField, Button } from '@mui/material';
+import IconButton from "@material-ui/core/IconButton";
+import ClearIcon from '@mui/icons-material/Clear';
+
+
+export default function AddressForm({ submitAddresses }) {
+
+    const [inputList, setInputList] = useState([{ name: "", address: "" }]);
+ 
+    // handle input change
+    const handleInputChange = (e, index) => {
+      const { name, value } = e.target;
+      const list = [...inputList];
+      list[index][name] = value;
+      setInputList(list);
+    };
+
+    // handle click event of the Remove button
+    const handleRemoveClick = index => {
+        const list = [...inputList];
+        list.splice(index, 1);
+        setInputList(list);
+    };
+ 
+    // handle click event of the Add button
+    const handleAddClick = () => {
+        setInputList([...inputList, { name: "", address: "" }]);
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log(inputList);
+        submitAddresses(inputList);
+    };
+
+    return(
+        <Container maxWidth="md" spacing={24}>
+            <Box sx={{ my: 4 }}>
+                {inputList.map((x, i) => {
+                    return (
+                        <div className="box" key={i}>
+                            <TextField
+                                name="name"
+                                placeholder="Enter Name"
+                                value={x.name}
+                                onChange={e => handleInputChange(e, i)}
+                            />
+                            <TextField
+                                // className="ml10"
+                                name="address"
+                                placeholder="Enter address"
+                                value={x.address}
+                                onChange={e => handleInputChange(e, i)}
+                            />
+                            <IconButton onClick={() => handleRemoveClick(i)} >
+                                <ClearIcon/>
+                            </IconButton>
+                        </div>
+                    );
+                })}
+                <Button fullWidth variant='contained' onClick={handleAddClick} key="Add person">Add person</Button>
+                <div style={{ marginTop: 20 }}>{JSON.stringify(inputList)}</div>
+                <Button fullWidth variant='contained' onClick={handleSubmit}>Submit addresses</Button>
+            </Box>
+        </Container>
+    );
+
+};
+
--- src/components/Engine.js
+++ src/components/Engine.js
@@ -1,84 +1,16 @@
 import * as React from 'react';
 import { useState } from 'react';
-import Container from '@mui/material/Container';
-import Typography from '@mui/material/Typography';
-import Box from '@mui/material/Box';
-import Link from '@mui/material/Link';
-import Grid from '@mui/material/Grid';
-import Input from '@mui/material/Input';
-import { TextField, Button } from '@mui/material';
-import IconButton from "@material-ui/core/IconButton";
-import ClearIcon from '@mui/icons-material/Clear';
-
-const n_person = 3;
-const initialValues = {};
-const persons = ["person1", "person2", "person3", "person4", "person5"];
-
-persons.forEach(person => { 
-    initialValues[person] = ""; 
-});
-
+import AddressForm from './AddressForm';
 
 export default function Engine() {
+    const [addresses, setAddresses] = useState([]);
 
-    const [inputList, setInputList] = useState([{ name: "", address: "" }]);
- 
-    // handle input change
-    const handleInputChange = (e, index) => {
-      const { name, value } = e.target;
-      const list = [...inputList];
-      list[index][name] = value;
-      setInputList(list);
-    };
-
-    // handle click event of the Remove button
-    const handleRemoveClick = index => {
-        const list = [...inputList];
-        list.splice(index, 1);
-        setInputList(list);
-    };
- 
-    // handle click event of the Add button
-    const handleAddClick = () => {
-        setInputList([...inputList, { name: "", address: "" }]);
-    };
-
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(values);
-    };
 
     return(
-        <Container maxWidth="md" spacing={24}>
-            <Box sx={{ my: 4, p:4}}>
-
-                {inputList.map((x, i) => {
-                    return (
-                        <div className="box">
-                            <TextField
-                                name="name"
-                                placeholder="Enter Name"
-                                value={x.name}
-                                onChange={e => handleInputChange(e, i)}
-                            />
-                            <TextField
-                                className="ml10"
-                                name="address"
-                                placeholder="Enter address"
-                                value={x.address}
-                                onChange={e => handleInputChange(e, i)}
-                            />
-                            <IconButton>
-                                <ClearIcon onClick={() => handleRemoveClick(i)} />
-                            </IconButton>
-                        </div>
-                    );
-                })}
-                <Button fullWidth variant='contained' onClick={handleAddClick}>Add person</Button>
-                <div style={{ marginTop: 20 }}>{JSON.stringify(inputList)}</div>
-            </Box>
-        </Container>
+        // <AddressForm submitAddresses={inputList => setAddresses(inputList)} />
+        <div>
+            <AddressForm submitAddresses={inputList => setAddresses(inputList)} />
+            <div style={{ marginTop: 20 }}>{JSON.stringify(addresses)}</div>
+        </div>
     );
-
 };
-
